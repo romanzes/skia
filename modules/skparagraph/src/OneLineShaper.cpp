d443b70d7fc43556880e05df9a80d4719bbde86f
Roman Petrenko <roman@canva.com>
2020-09-08 17:37:19 +1000

    set embolden

--- modules/skparagraph/src/OneLineShaper.cpp
+++ modules/skparagraph/src/OneLineShaper.cpp
@@ -592,7 +592,7 @@ bool OneLineShaper::shape() {
                 SkFont font(std::move(typeface), block.fStyle.getFontSize());
                 font.setEdging(SkFont::Edging::kAntiAlias);
                 font.setHinting(SkFontHinting::kNone);
-                font.setSubpixel(false);
+                font.setSubpixel(true);
 
                 // Apply fake bold and/or italic settings to the font if the
                 // typeface's attributes do not match the intended font style.
@@ -603,7 +603,7 @@ bool OneLineShaper::shape() {
                 bool fakeItalic =
                     block.fStyle.getFontStyle().slant() == SkFontStyle::kItalic_Slant &&
                     font.getTypeface()->fontStyle().slant() != SkFontStyle::kItalic_Slant;
-                font.setEmbolden(fakeBold);
+                font.setEmbolden(true);
                 font.setSkewX(fakeItalic ? -SK_Scalar1 / 4 : 0);
 
                 // Walk through all the currently unresolved blocks
