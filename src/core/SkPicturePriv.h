3cd129f3b2094780d384ae62570d87e2eeb14058
Roman Petrenko <roman@canva.com>
2020-10-12 12:58:23 +1100

    Update to m85-0.34.2 (#3)

--- src/core/SkPicturePriv.h
+++ src/core/SkPicturePriv.h
@@ -75,6 +75,8 @@ public:
     // V72: SkColorFilter_Matrix domain (rgba vs. hsla)
     // V73: Use SkColor4f in per-edge AA quad API
     // V74: MorphologyImageFilter internal radius is SkScaler
+    // V75: SkVertices switched from unsafe use of SkReader32 to SkReadBuffer (like everything else)
+    // V76: Add filtering enum to ImageShader
 
     enum Version {
         kTileModeInBlurImageFilter_Version  = 56,
@@ -96,10 +98,12 @@ public:
         kMatrixColorFilterDomain_Version    = 72,
         kEdgeAAQuadColor4f_Version          = 73,
         kMorphologyTakesScalar_Version      = 74,
+        kVerticesUseReadBuffer_Version      = 75,
+        kFilteringInImageShader_Version     = 76,
 
         // Only SKPs within the min/current picture version range (inclusive) can be read.
         kMin_Version     = kTileModeInBlurImageFilter_Version,
-        kCurrent_Version = kMorphologyTakesScalar_Version
+        kCurrent_Version = kFilteringInImageShader_Version
     };
 
     static_assert(kMin_Version <= 62, "Remove kFontAxes_bad from SkFontDescriptor.cpp");
