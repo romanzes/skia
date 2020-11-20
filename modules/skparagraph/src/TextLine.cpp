b88d6fa08ce8e6aa270e30435c12d65bccd5fb17
Roman Petrenko <roman@canva.com>
2020-11-20 20:03:06 +1100

    log

--- modules/skparagraph/src/TextLine.cpp
+++ modules/skparagraph/src/TextLine.cpp
@@ -376,6 +376,7 @@ void TextLine::paintText(SkCanvas* canvas, TextRange textRange, const TextStyle&
     }
 
     SkScalar correctedBaseline = SkScalarFloorToScalar(this->baseline() + 0.5);
+    SkDebugf("TextLine::paintText: offset().fX: %g\n", this->offset().fX);
     SkDebugf("TextLine::paintText: fTextShift: %g\n", context.fTextShift);
     canvas->drawTextBlob(builder.make(),
         this->offset().fX + context.fTextShift, this->offset().fY + correctedBaseline, paint);
