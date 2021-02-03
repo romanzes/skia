062532594c7353484ee9eae647620ce91cb102d1
Roman Petrenko <roman@canva.com>
2021-02-03 17:11:40 +1100

    .

--- src/core/SkCanvas.cpp
+++ src/core/SkCanvas.cpp
@@ -2583,7 +2583,7 @@ void SkCanvas::onDrawImageLattice(const SkImage* image, const Lattice& lattice, 
 
 void SkCanvas::onDrawTextBlob(const SkTextBlob* blob, SkScalar x, SkScalar y,
                               const SkPaint& paint) {
-    SkMatrix debug_matrix = canvas->getTotalMatrix();
+    SkMatrix debug_matrix = this->getTotalMatrix();
     SkDebugf("ParagraphImpl::paint: translate: %g, %g, position: %g, %g, total: %g, %g\n", debug_matrix.getTranslateX(), debug_matrix.getTranslateY(), x, y, debug_matrix.getTranslateX() + x, debug_matrix.getTranslateY() + y);
     SkRect storage;
     const SkRect* bounds = nullptr;
