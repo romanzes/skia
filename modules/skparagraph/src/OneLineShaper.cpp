7ec3daca02a2cafb00d4e752e82e58c635c32841
Roman Petrenko <roman@canva.com>
2021-05-18 10:39:22 +1000

    log

--- modules/skparagraph/src/OneLineShaper.cpp
+++ modules/skparagraph/src/OneLineShaper.cpp
@@ -569,8 +569,8 @@ bool OneLineShaper::shape() {
 
             matchResolvedFonts(block.fStyle, [&](sk_sp<SkTypeface> typeface) {
                 SkString familyName;
-                typeface.getFamilyName(*familyName);
-                SkDebugf("familyName: %s\n", familyName);
+                typeface->getFamilyName(*familyName);
+                SkDebugf("familyName: %s\n", familyName.writable_str());
 
                 // Create one more font to try
                 SkFont font(std::move(typeface), block.fStyle.getFontSize());
