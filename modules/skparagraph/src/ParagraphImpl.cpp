7bdcca9076c67f67d863779c3278827ea2ac0bb6
Roman Petrenko <romanzesp@gmail.com>
2022-01-17 11:54:07 +1100

    [CA-21705] Add soft break exemptions (#17)

--- modules/skparagraph/src/ParagraphImpl.cpp
+++ modules/skparagraph/src/ParagraphImpl.cpp
@@ -382,6 +382,14 @@ Cluster::Cluster(ParagraphImpl* owner,
     fIsIntraWordBreak = intraWordBreakLen == fTextRange.width();
     fIsHardBreak = fOwner->codeUnitHasProperty(fTextRange.end,
                                                SkUnicode::CodeUnitFlags::kHardLineBreakBefore);
+    // NON-SKIA-UPSTREAMED CHANGE
+    // Some of the symbols that Chrome doesn't recognize to be soft breaks,
+    // are soft breaks in Skia by default
+    fIsSoftBreakExemption = *ch == 0x21 // Exclamation mark (!)
+                           || *ch == 0x2F // Forward slash (/)
+                           || *ch == 0x7C // Vertical bar (|)
+                           || *ch == 0x7D; // Right brace (})
+    // END OF NON-SKIA-UPSTREAMED CHANGE
 }
 
 SkScalar Run::calculateWidth(size_t start, size_t end, bool clip) const {
