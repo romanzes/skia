3a4706a048a7cfcfc11cde3ed7aaffa2fe119e09
Roman Petrenko <romanzesp@gmail.com>
2022-01-17 11:54:07 +1100

    [CA-21705] Add soft break exemptions (#17)

--- modules/skparagraph/src/Run.cpp
+++ modules/skparagraph/src/Run.cpp
@@ -316,6 +316,11 @@ SkFont Cluster::font() const {
 }
 
 bool Cluster::isSoftBreak() const {
+    // NON-SKIA-UPSTREAMED CHANGE
+    if (isSoftBreakExemption()) {
+        return false;
+    }
+    // END OF NON-SKIA-UPSTREAMED CHANGE
     return fOwner->codeUnitHasProperty(fTextRange.end, CodeUnitFlags::kSoftLineBreakBefore);
 }
 
