413d45487d014c7d9c7d30010a0449b719459972
Roman Petrenko <roman@canva.com>
2022-03-22 18:31:28 +1100

    .

--- modules/skparagraph/src/TypefaceFontProvider.cpp
+++ modules/skparagraph/src/TypefaceFontProvider.cpp
@@ -91,10 +91,10 @@ void LazyTypefaceFontProvider::onGetFamilyName(int index, SkString* familyName) 
 SkFontStyleSet* LazyTypefaceFontProvider::onMatchFamily(const char familyName[]) const {
     SkString* found = fRegisteredFamilies.find(SkString(familyName));
     if (found) {
-        sk_sp<TypefaceFontStyleSet> styleSet = sk_make_sp<TypefaceFontStyleSet>(familyName);
+        sk_sp<SkFontStyleSet> styleSet = sk_make_sp<TypefaceFontStyleSet>(familyName);
         sk_sp<SkTypeface> typeface = makeFromFile(found->c_str(), 0);
         styleSet->appendTypeface(std::move(typeface));
-        return styleSet->ref();
+        return styleSet;
     }
     return nullptr;
 }
