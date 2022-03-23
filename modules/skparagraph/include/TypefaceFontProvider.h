63364ad75cbdb11217e9d008805144b38547945a
Roman Petrenko <roman@canva.com>
2022-03-23 15:26:33 +1100

    .

--- modules/skparagraph/include/TypefaceFontProvider.h
+++ modules/skparagraph/include/TypefaceFontProvider.h
@@ -113,7 +113,7 @@ public:
 
 private:
     SkTHashMap<SkString, SkString> fRegisteredFamilies;
-    SkTHashMap<SkString, sk_sp<TypefaceFontStyleSet>> fLoadedFamilies;
+    mutable SkTHashMap<SkString, sk_sp<TypefaceFontStyleSet>> fLoadedFamilies;
     SkTArray<SkString> fFamilyNames;
 };
 
