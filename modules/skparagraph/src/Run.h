46575a4636833500bc08811f4ad6f914ea941026
Roman Petrenko <roman@canva.com>
2022-08-26 11:53:19 +1000

    Revert "[CA-21705] Add soft break exemptions (#17)"
    This reverts commit da83814a244d4536bbc57ec2ceba5a25afe3e52f.

--- modules/skparagraph/src/Run.h
+++ modules/skparagraph/src/Run.h
@@ -296,7 +296,7 @@ public:
     bool isIntraWordBreak() const { return fIsIntraWordBreak; }
     bool isHardBreak() const { return fIsHardBreak; }
     // NON-SKIA-UPSTREAMED CHANGE
-    bool isSoftBreakExemption() const { return fIsSoftBreakExemption; }
+    bool isChromeBreak() const { return fIsChromeBreak; }
     // END OF NON-SKIA-UPSTREAMED CHANGE
 
     bool isSoftBreak() const;
@@ -352,7 +352,7 @@ private:
     bool fIsIntraWordBreak;
     bool fIsHardBreak;
     // NON-SKIA-UPSTREAMED CHANGE
-    bool fIsSoftBreakExemption;
+    bool fIsChromeBreak;
     // END OF NON-SKIA-UPSTREAMED CHANGE
 };
 
