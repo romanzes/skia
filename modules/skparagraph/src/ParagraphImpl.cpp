1053482e3df78faf254377b60bec265c5fd23c27
Roman Petrenko <roman@canva.com>
2022-08-29 09:43:10 +1000

    Revert "Revert "Remove paragraph width rounding (#11)""
    This reverts commit 0e91bd3253b4bbe1342d36572af109ad2dcaa09f.

--- modules/skparagraph/src/ParagraphImpl.cpp
+++ modules/skparagraph/src/ParagraphImpl.cpp
@@ -111,8 +111,13 @@ int32_t ParagraphImpl::unresolvedGlyphs() {
 
 void ParagraphImpl::layout(SkScalar rawWidth) {
 
+    // NON-SKIA-UPSTREAMED CHANGE
+    /*
     // TODO: This rounding is done to match Flutter tests. Must be removed...
     auto floorWidth = SkScalarFloorToScalar(rawWidth);
+    */
+    auto floorWidth = rawWidth;
+    // END OF NON-SKIA-UPSTREAMED CHANGE
 
     if ((!SkScalarIsFinite(rawWidth) || fLongestLine <= floorWidth) &&
         fState >= kLineBroken &&
@@ -192,6 +197,8 @@ void ParagraphImpl::layout(SkScalar rawWidth) {
     this->fOldWidth = floorWidth;
     this->fOldHeight = this->fHeight;
 
+    // NON-SKIA-UPSTREAMED CHANGE
+    /*
     // TODO: This rounding is done to match Flutter tests. Must be removed...
     fMinIntrinsicWidth = littleRound(fMinIntrinsicWidth);
     fMaxIntrinsicWidth = littleRound(fMaxIntrinsicWidth);
@@ -200,7 +207,8 @@ void ParagraphImpl::layout(SkScalar rawWidth) {
     if (fParagraphStyle.getMaxLines() == 1 ||
         (fParagraphStyle.unlimited_lines() && fParagraphStyle.ellipsized())) {
         fMinIntrinsicWidth = fMaxIntrinsicWidth;
-    }
+    } */
+    // END OF NON-SKIA-UPSTREAMED CHANGE
 
     // TODO: Since min and max are calculated differently it's possible to get a rounding error
     //  that would make min > max. Sort it out later, make it the same for now
