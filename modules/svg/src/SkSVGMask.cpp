a435255eb76f01278828e1748d1f4658fa15ca07
Roman Petrenko <roman@canva.com>
2023-12-11 17:16:07 +1100

    .

--- modules/svg/src/SkSVGMask.cpp
+++ modules/svg/src/SkSVGMask.cpp
@@ -29,6 +29,7 @@ SkRect SkSVGMask::bounds(const SkSVGRenderContext& ctx) const {
 }
 
 void SkSVGMask::renderMask(const SkSVGRenderContext& ctx) const {
+    SkDebugf("SkSVGMask::renderMask\n");
     // https://www.w3.org/TR/SVG11/masking.html#Masking
 
     // Propagate any inherited properties that may impact mask effect behavior (e.g.
@@ -48,10 +49,6 @@ void SkSVGMask::renderMask(const SkSVGRenderContext& ctx) const {
     mask_filter.setColorFilter(
                 SkColorFilters::Compose(SkLumaColorFilter::Make(), std::move(ci_filter)));
 
-    const auto obbt = ctx.transformForCurrentOBB(fMaskContentUnits);
-    lctx.canvas()->translate(obbt.offset.x, obbt.offset.y);
-    lctx.canvas()->scale(obbt.scale.x, obbt.scale.y);
-
     // Mask color filter layer.
     // Note: We could avoid this extra layer if we invert the stacking order
     // (mask/content -> content/mask, kSrcIn -> kDstIn) and apply the filter
@@ -59,6 +56,10 @@ void SkSVGMask::renderMask(const SkSVGRenderContext& ctx) const {
     // until after node content, which introduces extra state/complexity.
     // Something to consider if masking performance ever becomes an issue.
     lctx.canvas()->saveLayer(nullptr, &mask_filter);
+
+    const auto obbt = ctx.transformForCurrentOBB(fMaskContentUnits);
+    lctx.canvas()->translate(obbt.offset.x, obbt.offset.y);
+    lctx.canvas()->scale(obbt.scale.x, obbt.scale.y);
 
     for (const auto& child : fChildren) {
         child->render(lctx);
