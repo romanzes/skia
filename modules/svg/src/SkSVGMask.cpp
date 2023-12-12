1cae101482f1aea64e508308597fe2b7ac6be67c
Roman Petrenko <roman@canva.com>
2023-12-12 11:02:38 +1100

    .

--- modules/svg/src/SkSVGMask.cpp
+++ modules/svg/src/SkSVGMask.cpp
@@ -27,7 +27,7 @@ bool SkSVGMask::parseAndSetAttribute(const char* n, const char* v) {
 SkRect SkSVGMask::bounds(const SkSVGRenderContext& ctx) const {
     SkDebugf("SkSVGMask::bounds: resolveOBBRect(%f, %f, %f, %f)\n", fX, fY, fWidth, fHeight);
     auto result = ctx.resolveOBBRect(fX, fY, fWidth, fHeight, fMaskUnits);
-    SkDebugf("SkSVGMask::bounds: result: \n", result.x(), result.y(), result.width(), result.height());
+    SkDebugf("SkSVGMask::bounds: result: %f, %f, %f, %f\n", result.x(), result.y(), result.width(), result.height());
     return result;
 }
 
